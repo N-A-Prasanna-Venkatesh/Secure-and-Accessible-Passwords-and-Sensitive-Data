59dd01ca0c5b3490f6d22a831a7d69c8cec1b63f
N-A-Prasanna-Venkatesh <52624854+N-A-Prasanna-Venkatesh@users.noreply.github.com>
2021-05-11 14:17:13 +0530

    before implementaion of random shift(before 5.0 version)

---
+++ app/src/main/java/secure/alarm/app/Options.java
@@ -0,0 +1,14 @@
+package secure.alarm.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
